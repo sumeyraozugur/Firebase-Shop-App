59a205bfbd567dfb4e021a71b397148ab6ca4c3e
sumeyra <ozugursumeyra@gmail.com>
2022-10-22 17:12:46 +0300

    AddProduct and Home Page are designed

--- app/src/main/java/com/sum/shop/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sum/shop/ui/home/HomeFragment.kt
@@ -1,25 +1,28 @@
 package com.sum.shop.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.sum.shop.R
 import com.sum.shop.databinding.FragmentHomeBinding
 import com.sum.shop.delegate.viewBinding
+import com.sum.shop.utils.sent
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivClickAddClothes.setOnClickListener {
+            Navigation.sent(it, R.id.action_homeFragment_to_addProductFragment)
+        }
+
+        binding.tvSeeAll.setOnClickListener {
+            Navigation.sent(it, R.id.action_homeFragment_to_productDetailFragment)
+
+        }
     }
-
-
 }
